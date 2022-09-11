3d9014e2e9ed48dc9bf33e44ac83eac60b0418fe
frankQDU <57973160+frankQDU@users.noreply.github.com>
2022-09-11 17:56:32 +0800

    add img2vec function

--- utils.py
+++ utils.py
@@ -12,6 +12,36 @@ import xgboost as xgb
 from gensim.models import Word2Vec
 
 
+def fe_ima2vec(df, img_path, img_col, filename_extension, model,layer_output_size=128, sub_path = None):
+    
+    import pandas as pd
+    import torch
+    from img2vec_pytorch import Img2Vec
+    from PIL import Image
+    from tqdm import tqdm
+
+    img2vec = Img2Vec(cuda=(torch.cuda.is_available())   , model=model,layer_output_size=layer_output_size)
+
+    fe_img = []
+    for idx in tqdm(range(len(df))):
+        path = f'{img_path}/{df.loc[idx, img_col]}.{filename_extension}'
+        if sub_path:
+            path = f'{img_path}/{df.loc[idx, sub_path]}/{df.loc[idx, img_col]}.{filename_extension}'
+        try:
+            img = Image.open(path)
+            vec = img2vec.get_vec(img)
+        except:
+            vec = [None] * 512
+        fe_img.append(vec)
+
+    fe_img = pd.DataFrame(fe_img)
+    fe_img.columns = ['img_vec_' + str(i) for i in range(fe_img.shape[1])]
+    return fe_img
+
+
+
+
+
 import datetime
 
 
